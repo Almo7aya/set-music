d15e504d534831d53381f43b11cc0b94ebaf9066
Ali Almohaya <alialmohaya@gmail.com>
2018-03-19 19:36:59 +0300

    add navbar component

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+import Navbar from '../Navbar/Navbar';
+
 export default class App extends Component {
 
     render() {
         return (<div>
+            <Navbar />
             <h1>App Works</h1>
         </div>);
     };
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Navbar.css';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+        <nav>
+            <h1>Navbar Works</h1>
+        </nav>);
+    };
+}
