2612563d60ee53d25f8de31465dfde50e76e4a67
Ali Almohaya <alialmohaya@gmail.com>
2018-03-19 19:27:07 +0300

    add App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// global styles
+import '../node_modules/normalize.css/normalize.css';
 
+// Component imports 
+import App from './component/App/App';
 
-ReactDOM.render( <div>Hello</div>, document.getElementById('root'));
+ReactDOM.render( 
+    
+    <App/>
+
+    , document.getElementById('root'));
